bfc771dadb6fcf14cf3d7675465755ecec7b2040
Conrad Buck <conartist6@gmail.com>
2024-02-22 12:24:40 -0700

    Initial commit

---
+++ test/runner.js
@@ -0,0 +1,107 @@
+import { expect } from 'expect';
+import { evaluateSync, Context } from '@bablr/agast-vm';
+import { createPassthroughStrategy } from '@bablr/generator-class-trampoline/passthrough';
+import {
+  buildFragmentOpenTag,
+  buildNodeOpenTag,
+  buildNodeCloseTag,
+  buildFragmentCloseTag,
+  buildReference,
+  buildLiteral,
+} from '@bablr/agast-helpers/builders';
+
+export const runTest = (testCase) => {
+  const resultTokens = [
+    ...evaluateSync(Context.create(), createPassthroughStrategy(testCase.tokens)),
+  ];
+
+  expect(resultTokens).toEqual(testCase.tokens);
+};
+
+runTest({
+  tokens: [buildFragmentOpenTag(), buildFragmentCloseTag()],
+});
+
+runTest({
+  tokens: [
+    buildFragmentOpenTag(),
+    buildReference('node', false),
+    buildNodeOpenTag({}, 'Node'),
+    buildLiteral('hello world'),
+    buildNodeCloseTag('Node'),
+    buildFragmentCloseTag(),
+  ],
+});
+
+expect(() => {
+  runTest({
+    tokens: [
+      buildFragmentOpenTag(),
+      buildNodeOpenTag({}, 'Node'),
+      buildNodeCloseTag(),
+      buildFragmentCloseTag(),
+    ],
+  });
+}).toThrow();
+
+// expect(() => {
+runTest({
+  tokens: [buildFragmentOpenTag({ trivia: true }), buildFragmentCloseTag()],
+});
+// }).toThrow();
+
+expect(() => {
+  runTest({
+    tokens: [
+      buildFragmentOpenTag(),
+      // not matching anything: should fail
+      buildFragmentOpenTag({ trivia: true }),
+      buildFragmentCloseTag(),
+      buildFragmentCloseTag(),
+    ],
+  });
+}).toThrow();
+
+runTest({
+  tokens: [
+    buildFragmentOpenTag(),
+    buildFragmentOpenTag({ trivia: true }),
+    buildLiteral(' '),
+    buildFragmentCloseTag(),
+    buildFragmentCloseTag(),
+  ],
+});
+
+runTest({
+  tokens: [
+    buildFragmentOpenTag(),
+    buildNodeOpenTag({ intrinsic: true }, 'Keyword'),
+    buildLiteral('true'),
+    buildNodeCloseTag(),
+    buildFragmentCloseTag(),
+  ],
+});
+
+expect(() => {
+  runTest({
+    tokens: [
+      buildFragmentOpenTag(),
+      buildNodeOpenTag({ intrinsic: true }, 'Node'),
+      buildNodeCloseTag(),
+      buildFragmentCloseTag(),
+    ],
+  });
+}).toThrow();
+
+expect(() => {
+  runTest({
+    tokens: [
+      buildFragmentOpenTag(),
+      buildNodeOpenTag({ intrinsic: true }, 'Node'),
+      buildLiteral('='),
+      buildLiteral('>'),
+      buildNodeCloseTag(),
+      buildFragmentCloseTag(),
+    ],
+  });
+}).toThrow();
