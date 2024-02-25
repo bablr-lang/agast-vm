720c8f12f7d4b65f183162ac964db9597198c09a
Conrad Buck <conartist6@gmail.com>
2024-02-25 16:30:18 -0700

    documents

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,7 @@
 import { expect } from 'expect';
 import { evaluateSync, Context } from '@bablr/agast-vm';
 import { createPassthroughStrategy } from '@bablr/agast-vm-strategy-passthrough';
+import { enhanceStrategyWithDebugLogging } from '@bablr/strategy_enhancer-debug-log';
 import {
   buildFragmentOpenTag,
   buildNodeOpenTag,
@@ -12,7 +13,10 @@ import {
 
 export const runTest = (testCase) => {
   const resultTokens = [
-    ...evaluateSync(Context.create(), createPassthroughStrategy(testCase.tokens)),
+    ...evaluateSync(
+      Context.create(),
+      enhanceStrategyWithDebugLogging(createPassthroughStrategy(testCase.tokens)),
+    ),
   ];
 
   expect(resultTokens).toEqual(testCase.tokens);
@@ -66,10 +70,27 @@ runTest({
   tokens: [
     buildFragmentOpenTag(),
     buildFragmentOpenTag({ trivia: true }),
+    buildReference('children', true),
+    buildNodeOpenTag('Blank'),
     buildLiteral(' '),
+    buildNodeCloseTag(),
     buildFragmentCloseTag(),
     buildFragmentCloseTag(),
   ],
+});
+
+runTest({
+  tokens: [
+    buildFragmentOpenTag(),
+    buildNodeOpenTag({ trivia: true }, 'Blank'),
+    buildLiteral(' '),
+    buildNodeCloseTag(),
+    buildFragmentCloseTag(),
+  ],
+});
+
+runTest({
+  tokens: [buildNodeOpenTag({ trivia: true }, 'Blank'), buildLiteral(' '), buildNodeCloseTag()],
 });
 
 runTest({
