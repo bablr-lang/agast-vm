c30c8eb041224d0bf9cb45f43ac57208de0e71e6
Conrad Buck <conartist6@gmail.com>
2024-02-27 09:06:39 -0700

    Fix test runner

--- test/runner.js
+++ test/runner.js
@@ -3,127 +3,125 @@ import { evaluateSync, Context } from '@bablr/agast-vm';
 import { createPassthroughStrategy } from '@bablr/agast-vm-strategy-passthrough';
 import { enhanceStrategyWithDebugLogging } from '@bablr/strategy_enhancer-debug-log';
 
-import {
-  buildFragmentOpenTag,
-  buildNodeOpenTag,
-  buildNodeCloseTag,
-  buildFragmentCloseTag,
-  buildReference,
-  buildLiteral,
-} from '@bablr/agast-vm-helpers/builders';
+import * as builders from '@bablr/agast-helpers/builders';
+import * as sourceBuilders from '@bablr/agast-vm-helpers/builders';
 
 export const runTest = (testCase) => {
   const resultTokens = [
     ...evaluateSync(
       Context.create(),
-      enhanceStrategyWithDebugLogging(createPassthroughStrategy(testCase.tokens)),
+      enhanceStrategyWithDebugLogging(createPassthroughStrategy(testCase.tokens(sourceBuilders))),
     ),
   ];
 
-  expect(resultTokens).toEqual(testCase.tokens);
+  expect(resultTokens).toEqual(testCase.tokens(builders));
 };
 
 runTest({
-  tokens: [buildFragmentOpenTag(), buildFragmentCloseTag()],
+  tokens: (b) => [b.buildFragmentOpenTag(), b.buildFragmentCloseTag()],
 });
 
 runTest({
-  tokens: [
-    buildFragmentOpenTag(),
-    buildReference('node', false),
-    buildNodeOpenTag({}, 'Node'),
-    buildLiteral('hello world'),
-    buildNodeCloseTag('Node'),
-    buildFragmentCloseTag(),
+  tokens: (b) => [
+    b.buildFragmentOpenTag(),
+    b.buildReference('node', false),
+    b.buildNodeOpenTag({}, 'Node'),
+    b.buildLiteral('hello world'),
+    b.buildNodeCloseTag('Node'),
+    b.buildFragmentCloseTag(),
   ],
 });
 
 expect(() => {
   runTest({
-    tokens: [
-      buildFragmentOpenTag(),
-      buildNodeOpenTag({}, 'Node'),
-      buildNodeCloseTag(),
-      buildFragmentCloseTag(),
+    tokens: (b) => [
+      b.buildFragmentOpenTag(),
+      b.buildNodeOpenTag({}, 'Node'),
+      b.buildNodeCloseTag(),
+      b.buildFragmentCloseTag(),
     ],
   });
 }).toThrow();
 
 // expect(() => {
 runTest({
-  tokens: [buildFragmentOpenTag({ trivia: true }), buildFragmentCloseTag()],
+  tokens: (b) => [b.buildFragmentOpenTag({ trivia: true }), b.buildFragmentCloseTag()],
 });
 // }).toThrow();
 
 expect(() => {
   runTest({
-    tokens: [
-      buildFragmentOpenTag(),
+    tokens: (b) => [
+      b.buildFragmentOpenTag(),
       // not matching anything: should fail
-      buildFragmentOpenTag({ trivia: true }),
-      buildFragmentCloseTag(),
-      buildFragmentCloseTag(),
+      b.buildFragmentOpenTag({ trivia: true }),
+      b.buildFragmentCloseTag(),
+      b.buildFragmentCloseTag(),
     ],
   });
 }).toThrow();
 
 runTest({
-  tokens: [
-    buildFragmentOpenTag(),
-    buildFragmentOpenTag({ trivia: true }),
-    buildReference('children', true),
-    buildNodeOpenTag('Blank'),
-    buildLiteral(' '),
-    buildNodeCloseTag(),
-    buildFragmentCloseTag(),
-    buildFragmentCloseTag(),
+  tokens: (b) => [
+    b.buildFragmentOpenTag(),
+    b.buildFragmentOpenTag({ trivia: true }),
+    b.buildReference('children', true),
+    b.buildNodeOpenTag('Blank'),
+    b.buildLiteral(' '),
+    b.buildNodeCloseTag(),
+    b.buildFragmentCloseTag(),
+    b.buildFragmentCloseTag(),
   ],
 });
 
 runTest({
-  tokens: [
-    buildFragmentOpenTag(),
-    buildNodeOpenTag({ trivia: true }, 'Blank'),
-    buildLiteral(' '),
-    buildNodeCloseTag(),
-    buildFragmentCloseTag(),
+  tokens: (b) => [
+    b.buildFragmentOpenTag(),
+    b.buildNodeOpenTag({ trivia: true }, 'Blank'),
+    b.buildLiteral(' '),
+    b.buildNodeCloseTag(),
+    b.buildFragmentCloseTag(),
   ],
 });
 
 runTest({
-  tokens: [buildNodeOpenTag({ trivia: true }, 'Blank'), buildLiteral(' '), buildNodeCloseTag()],
+  tokens: (b) => [
+    b.buildNodeOpenTag({ trivia: true }, 'Blank'),
+    b.buildLiteral(' '),
+    b.buildNodeCloseTag(),
+  ],
 });
 
 runTest({
-  tokens: [
-    buildFragmentOpenTag(),
-    buildNodeOpenTag({ intrinsic: true }, 'Keyword'),
-    buildLiteral('true'),
-    buildNodeCloseTag(),
-    buildFragmentCloseTag(),
+  tokens: (b) => [
+    b.buildFragmentOpenTag(),
+    b.buildNodeOpenTag({ intrinsic: true }, 'Keyword'),
+    b.buildLiteral('true'),
+    b.buildNodeCloseTag(),
+    b.buildFragmentCloseTag(),
   ],
 });
 
 expect(() => {
   runTest({
-    tokens: [
-      buildFragmentOpenTag(),
-      buildNodeOpenTag({ intrinsic: true }, 'Node'),
-      buildNodeCloseTag(),
-      buildFragmentCloseTag(),
+    tokens: (b) => [
+      b.buildFragmentOpenTag(),
+      b.buildNodeOpenTag({ intrinsic: true }, 'Node'),
+      b.buildNodeCloseTag(),
+      b.buildFragmentCloseTag(),
     ],
   });
 }).toThrow();
 
 expect(() => {
   runTest({
-    tokens: [
-      buildFragmentOpenTag(),
-      buildNodeOpenTag({ intrinsic: true }, 'Node'),
-      buildLiteral('='),
-      buildLiteral('>'),
-      buildNodeCloseTag(),
-      buildFragmentCloseTag(),
+    tokens: (b) => [
+      b.buildFragmentOpenTag(),
+      b.buildNodeOpenTag({ intrinsic: true }, 'Node'),
+      b.buildLiteral('='),
+      b.buildLiteral('>'),
+      b.buildNodeCloseTag(),
+      b.buildFragmentCloseTag(),
     ],
   });
 }).toThrow();
